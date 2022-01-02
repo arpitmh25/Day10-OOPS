88c9f7932c1072ed9bcacb30e459e82309e8f820
Arpit <arpitmh23@gmail.com>
2022-01-02 22:30:33 +0530

    Implementing chnages

---
+++ src/com/bridgelabz/day10/EmpWageBuilderArray.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.day10;
+
+public class EmpWageBuilderArray {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+
+    private int numOfCompany = 0;
+    private CompanyEmpWage[] companyEmpWagesArray;
+
+    public EmpWageBuilderArray() {
+        companyEmpWagesArray = new CompanyEmpWage[5];
+    }
+
+    private void addCompanyEmpWage(String company, int empRatePerhour,
+                                   int numOfWorkingDays, int maxHoursPerMonth) {
+        companyEmpWagesArray[numOfCompany] = new CompanyEmpWage(company, empRatePerhour, numOfWorkingDays, maxHoursPerMonth);
+        numOfCompany++;
+    }
+
+    private void computeEmpWage() {
+        for (int i = 1; i <= numOfCompany; i++) {
+            companyEmpWagesArray[i].setTotalEmpwage(this.computeEmpWage(companyEmpWagesArray[i]));
+            System.out.println(companyEmpWagesArray[i]);
+        }
+    }
+
+    private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+        //variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        //computation
+        while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth &&
+                totalWorkingDays < companyEmpWage.numOfWorkingDays) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+        }
+        return totalEmpHrs * companyEmpWage.empRatePerHour;
+    }
+
+    public static void main(String[] args) {
+        EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+        empWageBuilder.addCompanyEmpWage("Future Retail", 26, 28, 80);
+        empWageBuilder.addCompanyEmpWage("Spancer", 23, 24, 100);
+        empWageBuilder.computeEmpWage();
+    }
+}
